68839b26538074821cbb3f6856339d26ef00fed5
TastyKayKs <KevinKay1990@Gmail.com>
2021-01-20 16:46:58 -0600

    Update Actions.ps1

--- Actions.ps1
+++ Actions.ps1
@@ -647,7 +647,7 @@ Function Actions{
                     }
                 }Else{
                     Try{
-                        $PHProc = @(PS $ProcSearchTerm -ErrorAction Stop | ?{$_.MainWindowHandle -ne 0})
+                        $PHProc = @(PS $ProcSearchTerm | ?{$_.MainWindowHandle -ne 0})
                         If(!$PHProc.Count){
                             $PHProc = @(PS | ?{$_.Id -notmatch $SyncHash.MouseIndPid} | ?{$_.MainWindowTitle -match $ProcSearchTerm})
                         }
